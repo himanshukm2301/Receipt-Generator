e4e51e4e763697374e1fae5abf48d143838cc86f
Himanshu Mishra <himanshukm2301@MacBook-Pro.local>
2023-07-22 02:25:28 +0530

    added new input fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,18 @@ export class AppComponent {
   paymentDate: string = '21/07/2023';
   invoiceDate: string = '21/07/2023';
   receiptNumber: number = 56;
-  
+  paymentMode: string = '';
+  purposeOfContribution: string = '';
+  transactionID: number = 0;
+
+  amountInWords: string = '';
 
   generatePDF() {
+
+    this.amountInWords = this.numberToWords(this.amount);
+    // console.log(this.amountInWords);
     console.log('generatePDF() called');
+
     console.log('userName:', this.userName);
     console.log('mobileNumber:', this.mobileNumber);
     console.log('address:', this.address);
@@ -26,6 +34,12 @@ export class AppComponent {
     console.log('amount:', this.amount);
     console.log('paymentDate:', this.paymentDate);
     console.log('invoiceDate:', this.invoiceDate);
+
+    console.log('receiptNumber:', this.receiptNumber);
+    console.log('paymentMode:', this.paymentMode);
+    console.log('purposeOfContribution:', this.purposeOfContribution);
+
+    console.log('transactionID:', this.transactionID);
     
 
     var props = {
@@ -108,9 +122,7 @@ export class AppComponent {
               // "Donation",
               `
 
-Received with thanks from Mr./Mrs./Ms ${this.userName} Rupees (in words) One Hundred only By Online 
-
-Payment dated ${this.paymentDate} with Transaction ID: 24234 on account of Advertisement.
+Received with thanks from Mr./Mrs./Ms ${this.userName} Rupees (in words) ${this.amountInWords} only By ${this.paymentMode} dated ${this.paymentDate} with Transaction ID: ${this.transactionID} on account of ${this.purposeOfContribution}.
 
 
 AMOUNT PAID: Rs ${this.amount}                                                                                            Treasurer Sign: Arun Kumar Mishra`,
@@ -143,6 +155,88 @@ BRANCH NAME: Nizampura, Vadodara
 
     console.log('pdfObject', pdfObject);
   }
+
+
+
+
+  numberToWords(num: number): string {
+    let arr = new Map();
+     arr.set(1000000000, "Billion");
+     arr.set(1000000, "Million");
+     arr.set(1000, "Thousand");
+     arr.set(100, "Hundred");
+     arr.set(10, "Ten");
+     arr.set(9, "Nine");
+     arr.set(8, "Eight");
+     arr.set(7, "Seven");
+     arr.set(6, "Six");
+     arr.set(5, "Five");
+     arr.set(4, "Four");
+     arr.set(3, "Three");
+     arr.set(2, "Two");
+     arr.set(1, "One");
+ 
+     let second_arr = new Map();
+     second_arr.set(9, "Ninety");
+     second_arr.set(8, "Eighty");
+     second_arr.set(7, "Seventy");
+     second_arr.set(6, "Sixty");
+     second_arr.set(5, "Fifty");
+     second_arr.set(4, "Forty");
+     second_arr.set(3, "Thirty");
+     second_arr.set(2, "Twenty");
+ 
+     let third_arr = new Map();
+     third_arr.set(9, "Nineteen");
+     third_arr.set(8, "Eighteen");
+     third_arr.set(7, "Seventeen");
+     third_arr.set(6, "Sixteen");
+     third_arr.set(5, "Fifteen");
+     third_arr.set(4, "Fourteen");
+     third_arr.set(3, "Thirteen");
+     third_arr.set(2, "Twelve");
+     third_arr.set(1, "Eleven");
+     third_arr.set(0, "Ten");
+ 
+     if(num === 0) return "Zero";
+     if(num <= 10) return arr.get(num);
+     if(num % 10 === 0 && num > 10 && num < 100) return second_arr.get(Math.floor(num / 10));
+      
+     let res: string = "";
+ 
+     function func(n:number):string {
+         let result: string = "";
+         let a = Math.floor(n / 100);
+         if(a > 0) {result += arr.get(a) + " " + "Hundred "; n %= 100};
+         let b = Math.floor(n / 10);
+         if(b > 1) {result += second_arr.get(b) + " "; n %= 10};
+         let c = Math.floor(n / 10);
+         if(c > 0) result += third_arr.get(n % 10) + " "; else if(n > 0) result += arr.get(n % 10) + " ";
+         return result;
+     }
+ 
+     for(let [key, elem] of arr) {
+         if(num >= key) {
+             let a:number = Math.floor(num / key);
+             if(a > 9) {
+                 res += func(a);
+             }
+             if(key === 10 && a > 1 && a <= 9) {
+                 res += second_arr.get(a) + " " + ((num % key > 0) ? arr.get(num % key) : "");
+                 return res.trim();
+             } else if(key === 10 && a === 1) {
+                 res += third_arr.get(num % key) + " ";
+                 return res.trim();
+             }
+             res += (0 < a && a <= 9 && key % 10 === 0 ? arr.get(a) + " " : "") + elem + " ";
+             num %= key;
+         }
+     }
+     return res.trim();
+ }
+
+
+
 }
 
 //Cash donation of Rs ${this.amount} made on ${this.paymentDate} by ${this.userName} to the Maithil Samaj Foundation
@@ -155,3 +249,12 @@ On behalf of the Mithila Samaj, I would like to express our sincere gratitude fo
 We are grateful for your support and we look forward to continuing to work with you to build a stronger and more vibrant Mithila community.     
 `
  */
+
+
+/*
+`
+
+Received with thanks from Mr./Mrs./Ms ${this.userName} Rupees (in words) ${this.amountInWords} only By ${this.paymentMode} dated ${this.paymentDate} with Transaction ID: ${this.transactionID} on account of ${this.purposeOfContribution}.
+
+
+AMOUNT PAID: Rs ${this.amount}                                                                                            Treasurer Sign: Arun Kumar Mishra` */
